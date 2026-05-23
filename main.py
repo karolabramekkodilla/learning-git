656b3fcff2450fde2b18cad263ebecda196a0c73
Karol Abramek <karol.abramek.kodilla@gmail.com>
2026-05-23 12:15:23 +0200

    Printing dictionary

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 shopping_dict = {'piekarnia' : ['chleb','pączek','bułki'],
                  'warzywniak' : ['marchew', 'seler', 'rulkola']}
+
+print('Lista zakupów')
+
+for key, value in shopping_dict.items():
+    print('Idę do ', key,', kupuje tu następujące rzeczy:', value)
