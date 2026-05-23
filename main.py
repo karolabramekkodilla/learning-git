471a1ed8bf762524fd97e157b0047ac619c6fc48
Karol Abramek <karol.abramek.kodilla@gmail.com>
2026-05-23 12:19:17 +0200

    Counting items to buy

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ shopping_dict = {'piekarnia' : ['chleb','pączek','bułki'],
 
 print('Lista zakupów')
 
+counter = 0
 for key, value in shopping_dict.items():
     capitalized_items = []
     for item in value:
         capitalized_items.append(item.capitalize())
+        counter += 1
 
     print('Idę do ', key.capitalize(),', kupuje tu następujące rzeczy:', capitalized_items)
+print('W sumie kupuję ',counter,' produktów')
