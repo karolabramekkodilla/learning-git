bd9e93d1293bb2311588de3573ff057a8cba3c75
karolabramekkodilla <karol.abramek.kodilla@gmail.com>
2026-05-24 08:53:58 +0200

    Create zadanie_2.py liczby podzielne przez 5

---
+++ zadanie_2.py
@@ -0,0 +1,12 @@
+liczby = []
+liczby_podzielne_przez_5 = []
+liczy_do_potegi_3 = []
+for i in range(0,101):
+    liczby.append(i)
+# print(liczby)
+for item in liczby:
+    if item%5 == 0:
+        liczby_podzielne_przez_5.append(item)
+print(liczby_podzielne_przez_5)
+print([item ** 3 for item in liczby_podzielne_przez_5])
+    
