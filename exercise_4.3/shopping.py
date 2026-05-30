2a1848b29a582fbf85012f823decd4956d6cd054
Karol Abramek <karol.abramek.kodilla@gmail.com>
2026-05-30 09:31:14 +0200

    test shopping

--- exercise_4.3/shopping.py
+++ exercise_4.3/shopping.py
@@ -1,4 +1,4 @@
-def shopping(items, payment='card', shop='local'):
+def shopping(items, payment='card', shop='local shop'):
     result = ""
     result = result + "Idę na zakupy do %s.\n" % shop
     result = result + "Kupię następujące rzeczy:\n"
@@ -7,6 +7,8 @@ def shopping(items, payment='card', shop='local'):
     result = result + "By zapłacić, używam %s." % payment
     return result
 
-items = ["cola", "whiskey", "lód"]
-text = shopping(items, 'card', 'small local shop')
-print(text)
+if __name__ == "__main__":
+    items_text = input("Podaj proszę produkty rozdzielone przecinkiem: ")
+    items = items_text.split(',')
+    shopping_result = shopping(items)
+    print(shopping_result)
