da9579f6e95cd93bf579e6246dc61d2f0f27599c
Karol Abramek <karol.abramek.kodilla@gmail.com>
2026-05-30 08:29:06 +0200

    Utworzenie funkcji palindrom

--- palindrom/palindrom.py
+++ palindrom/palindrom.py
@@ -1,2 +1,16 @@
 def check_if_word_is_palindrom(word = "word_to_check"):
-    pass
+    i = 0 
+    j = len(word) - 1  
+    is_palindrome = True  
+
+    while i < j:
+        if word[i] != word[j]:  
+            is_palindrome = False
+            break
+        i += 1
+        j -= 1
+
+    return is_palindrome
+
+print(check_if_word_is_palindrom(word="kajak"))
+print(check_if_word_is_palindrom(word="yo"))
