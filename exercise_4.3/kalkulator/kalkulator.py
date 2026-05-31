18c52aacc225c7acc8b303d6d22be5747717e9c2
Karol Abramek <karol.abramek.kodilla@gmail.com>
2026-05-31 10:20:40 +0200

    Utowrzenie funkcji dla kalkulatora

---
+++ exercise_4.3/kalkulator/kalkulator.py
@@ -0,0 +1,71 @@
+import os
+import logging
+os.makedirs("my_file_log", exist_ok=True)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="my_file_log/logfile.log")
+
+def add_numbers(args):
+    temp=args[0]
+    for i in args[1:]:
+        temp += i
+    logging.debug(f"Suma liczb {args} wynosi {temp}")
+    return sum
+
+def multiply_numbers(args):
+    temp=args[0]
+    for i in args[1:]:
+        temp *= i
+    logging.debug(f"Mnożenie liczb {args} wynosi {temp}")
+    return temp
+def divide_numbers(args):
+    temp=args[0]
+    for i in args[1:]:
+        temp /= i
+    logging.debug(f"Dzielenie liczb {args} wynosi {temp}")
+    return temp
+def subtract_numbers(args):
+    temp=args[0]
+    for i in args[1:]:
+        temp -= i
+    logging.debug(f"Odejmowanie liczb {args} wynosi {temp}")
+    return temp
+
+def input_two_numbers():
+    try:
+        a = float(input("Podaj składnik 1.\t"))
+        b = float(input("Podaj składnik 2.\t"))
+        return a,b
+    except ValueError:
+        logging.error("Podano wartość niebędącą liczbą")
+        print("Podana wartość nie jest liczbą")
+        return None
+    
+def input_many_numbers():
+    arguments=[]
+    value = None
+    try:
+        while value != "q":
+            value = input("Podaj liczbę do działania lub -> q - zakończ")
+            if value != "q":
+                arguments.append(value)
+            
+    except ValueError:
+        logging.error("Podano wartość niebędącą liczbą")
+        print("Podana wartość nie jest liczbą")
+
+
+if __name__ == "__main__":
+
+    case = input(
+        "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "
+    )
+
+    if case == "1":
+        pass
+    elif case == "2":
+        pass
+    elif case == "3":
+        pass
+    elif case == "4":
+        pass
+    else:
+        logging.debug("Wybrano nieprawidłowy case")
