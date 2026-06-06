e79a3a8eaeba8ebea08cd1a11b7f0e620f2bf234
Karol Abramek <karol.abramek.kodilla@gmail.com>
2026-06-06 08:49:23 +0200

    Dodanie @property full name lenght

--- exercise_5/InfoCard.py
+++ exercise_5/InfoCard.py
@@ -8,3 +8,10 @@ class InfoCard():
 
     def __str__(self):
         return f"{self.name} {self.surname} - {self.email}"
+    
+    def contact(self):
+        print(f"Kontaktuje się z {self.name} {self.surname} {self.email}")
+    
+    @property
+    def full_name_lenght(self):
+        return len(f"{self.name} {self.surname}")
